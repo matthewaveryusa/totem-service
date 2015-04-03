7dd2532a6cca0d0aa21450317aabefe88fac4228
matthewaveryusa <matthewaveryusa@gmail.com>
2015-04-03 13:55:22 -0400

    es6

--- src/express.js
+++ src/express.js
@@ -1,5 +1,5 @@
 'use strict';
-var _ = require('lodash'),
+const _ = require('lodash'),
   tools = require('totem-tools'),
   os = require('os');
 /**
@@ -11,105 +11,111 @@ var _ = require('lodash'),
  * @constructor
  */
 
-function ExpressRoutes(){
-  this.description = {
-    ping: 'returns a timestamped pong',
-    ok: 'returns string OK. for HAProxy high-level health-checks',
-    end: 'gracefully end the task',
-    crash: 'SIGTERM the task',
-    process: 'information about the process',
-    //TODO security issue
-    //env: 'environment variables the process is exposed to',
-    os: 'operating system details',
-    network: 'network interfaces available',
-    help: 'help file'
-  };
+class ExpressRoutes {
+  constructor() {
+    this.description = {
+      ping: 'returns a timestamped pong',
+      ok: 'returns string OK. for HAProxy high-level health-checks',
+      end: 'gracefully end the task',
+      crash: 'SIGTERM the task',
+      process: 'information about the process',
+      //TODO security issue
+      //env: 'environment variables the process is exposed to',
+      os: 'operating system details',
+      network: 'network interfaces available',
+      help: 'help file'
+    };
+  }
+
+  /**
+   * Ping route. Pings the service for a timestamp
+   * @param {external:express/Request} req
+   * @param {external:express/Response} res
+   * @example
+   * //sample response:
+   */
+
+  routeDetailsfunction () {
+    return this.description;
+  }
+
+  ping(req, res) {
+    res.json({'pong': tools.nowMs()});
+  }
+
+  ok(req, res) {
+    res.send('OK');
+  }
+
+  help(req, res) {
+    res.json(this.description);
+  }
+
+  crash(req, res) {
+    res.json({});
+    process.exit(1);
+  }
+
+  end(req, res) {
+    res.json({});
+    process.kill(process.pid, 'SIGTERM');
+  }
+
+  process(req, res) {
+    var mem = process.memoryUsage();
+    var humanMem = _.mapValues(mem, function (val) {
+      return tools.humanSize(val);
+    });
+    res.json({
+      'argv': process.argv,
+      'pid': process.pid,
+      'title': process.title,
+      'execArgv': process.execArgv,
+      'cwd': process.cwd(),
+      'arch': process.arch,
+      'platform': process.platform,
+      'memory': mem,
+      'humanMemory': humanMem,
+      'uptime': process.uptime(),
+      'umask': process.umask(),
+      'gid': process.getgid(),
+      'uid': process.getuid(),
+      'execPath': process.execPath
+    });
+  }
+
+  env(req, res) {
+    res.json(process.env);
+  }
+
+  os(req, res) {
+    var mem = {'total': os.totalmem(), 'free': os.freemem()};
+    var humanMem = _.mapValues(mem, function (val) {
+      return tools.humanSize(val);
+    });
+    var cpus = os.cpus();
+    res.json({
+      'hostname': os.hostname(),
+      'endianness': os.endianness(),
+      'type': os.type(),
+      'platform': os.platform(),
+      'release': os.release(),
+      'arch': os.arch(),
+      'CPUs': {'count': cpus.length, 'model': cpus[0].model, 'peed': cpus[0].speed},
+      'loadAvg': _.map(os.loadavg(), function (val) {
+        return val.toFixed(4);
+      }),
+      'memory': mem,
+      'humanMemory': humanMem,
+      'uptime': os.uptime(),
+      'tmpDir': os.tmpdir()
+    });
+  }
+
+  network(req, res) {
+    res.json(os.networkInterfaces());
+  }
 }
 
-ExpressRoutes.prototype.constructor = ExpressRoutes;
-
 exports.ExpressRoutes = ExpressRoutes;
-
-/**
- * Ping route. Pings the service for a timestamp
- * @param {external:express/Request} req
- * @param {external:express/Response} res
- * @example
- * //sample response: 
- */
-
-ExpressRoutes.prototype.routeDetails = function () {
-  return this.description;
-};
-
-ExpressRoutes.prototype.ping = function (req, res) {
-  res.json({'pong': tools.nowMs()});
-};
-
-ExpressRoutes.prototype.ok = function (req, res) {
-  res.send('OK');
-};
-
-ExpressRoutes.prototype.help = function(req,res) {
-  res.json(this.description);
-};
-
-ExpressRoutes.prototype.crash = function(req,res) {
-  res.json({});
-  process.exit(1);
-};
-
-ExpressRoutes.prototype.end = function(req,res) {
-  res.json({});
-  process.kill(process.pid, 'SIGTERM');
-};
-
-ExpressRoutes.prototype.process = function(req,res){
-  var mem = process.memoryUsage();
-  var humanMem = _.mapValues(mem,function(val){ return tools.humanSize(val);});
-  res.json({
-    'argv' : process.argv,
-    'pid' : process.pid,
-    'title': process.title,
-    'execArgv' : process.execArgv,
-    'cwd' : process.cwd(),
-    'arch': process.arch,
-    'platform' : process.platform,
-    'memory': mem,
-    'humanMemory': humanMem,
-    'uptime': process.uptime(),
-    'umask': process.umask(),
-    'gid': process.getgid(),
-    'uid': process.getuid(),
-    'execPath': process.execPath
-  });
-};
-
-ExpressRoutes.prototype.env = function(req,res) {
-  res.json(process.env);
-};
-
-ExpressRoutes.prototype.os = function(req,res){
-  var mem = { 'total': os.totalmem(), 'free': os.freemem()};
-  var humanMem = _.mapValues(mem,function(val){ return tools.humanSize(val);});
-  var cpus = os.cpus();
-  res.json({
-    'hostname': os.hostname(),
-    'endianness': os.endianness(),
-    'type': os.type(),
-    'platform': os.platform(),
-    'release': os.release(),
-    'arch': os.arch(),
-    'CPUs': {'count': cpus.length, 'model' : cpus[0].model, 'peed' : cpus[0].speed },
-    'loadAvg' : _.map(os.loadavg(),function(val){ return val.toFixed(4);}),
-    'memory': mem,
-    'humanMemory': humanMem,
-    'uptime': os.uptime(),
-    'tmpDir' : os.tmpdir()
-  });
-};
-
-ExpressRoutes.prototype.network = function(req,res){
-  res.json(os.networkInterfaces());
-};
 
--- src/service.js
+++ src/service.js
@@ -1,123 +1,125 @@
 'use strict';
 
-var _ = require('lodash'),
+const _ = require('lodash'),
   ExpressRoutes = require('./express.js').ExpressRoutes,
   express = require('express'),
   http = require('http'),
   log = require('tracer').console();
 
-exports.Service = Service;
 /**
  * @module libs/service
  */
 
-/**
- *
- * @constructor
- */
-
-function Service(options) {
-  var self = this;
-  this.options = options;
-  process.on('SIGTERM', function sigtermEvent() {
-    self.end();
-    setTimeout(function sigtermTimeout() { process.exit(0); },
-      self.sigtermTimeout || 0);
-  });
-}
-
-Service.prototype.constructor = Service;
-
-Service.prototype.connect = function() {
-  log.info('starting server');
-  var app = this.expressApp();
-  this.server = http.createServer(app);
-  this.setupServerEvents();
-  this.setupRoutes(app);
-  this.server.on('error', function httpServerError(err) {
-    log.error(err);
-    process.exit(1);
-  });
-  if(this.options.host === '*') {
-    this.server.listen(this.options.port);
-  } else {
-    this.server.listen(this.options.port, this.options.host);
-  }
-};
-
-Service.prototype.reconnect = function() {
-  var self = this;
-  this.disconnect(function reconnectCallback(){ self.connect(); });
-};
-
-Service.prototype.disconnect = function(callback) {
-  try {
-    this.server.close(callback);
-  } catch(e) {
-    log.error('disconnect throw',e);
-  }
-  _.forEach(this.serverConnections,function closeConnection(connection){
-    connection.destroy();
-  });
-};
-
-Service.prototype.setupServerEvents = function() {
-  this.serverConnections = {};
-  this.nextConnectionId = 0;
-  var self = this;
-  this.server.on('connection', function ConnectionEvent(connection) {
-
-    self.nextConnectionId++;
-    var id = self.nextConnectionId;
-    self.serverConnections[id] = connection;
-
-    connection.on('close', function CloseEvent() {
-      delete self.serverConnections[id];
+class Service {
+  constructor(options) {
+    var self = this;
+    this.options = options;
+    process.on('SIGTERM', function sigtermEvent() {
+      self.end();
+      setTimeout(function sigtermTimeout() {
+          process.exit(0);
+        },
+        self.sigtermTimeout || 0);
     });
-  });
-};
-
-Service.prototype.setupRoutes = function(app) {
-  app.use('/',function ApiRequestRoute(req, res, next) {
-    log.info('API request', req.path, JSON.stringify(req.body));
-    next();
-  });
-
-  app.use(function NotFoundRoute(req, res) {
-    log.info(404,req.path);
-    res.status(404);
-    res.json({'message': 'not found'});
-  });
-};
-
-Service.prototype.expressApp = function() {
-  var app = express(),
-    adminRoute = express.Router();
-  var routes = new ExpressRoutes();
-  app.disable('x-powered-by');
-  app.enable('trust proxy');
-
-  var serviceRouteCtx = function(route) {
-    return function ServiceRoute(req,res) { route(req,res); };
-  };
-
-  _.keys(routes.routeDetails()).forEach(function registerServiceRoute(route){
-    adminRoute.route('/'+route).get(serviceRouteCtx(routes[route].bind(routes)));
-  });
-
-  app.use('/_/', adminRoute);
-  return app;
-};
-
-Service.prototype.defaultCallback = function(err,data) {
-  if(err) {
-    log.error(err,data);
-    process.exit(1);
-    return;
   }
-  this.connect();
-};
 
-Service.prototype.end = function() {
-  this.disconnect();
-};
+
+  connect() {
+    log.info('starting server');
+    var app = this.expressApp();
+    this.server = http.createServer(app);
+    this.setupServerEvents();
+    this.setupRoutes(app);
+    this.server.on('error', function httpServerError(err) {
+      log.error(err);
+      process.exit(1);
+    });
+    if (this.options.host === '*') {
+      this.server.listen(this.options.port);
+    } else {
+      this.server.listen(this.options.port, this.options.host);
+    }
+  }
+
+  reconnect() {
+    var self = this;
+    this.disconnect(function reconnectCallback() {
+      self.connect();
+    });
+  }
+
+  disconnect(callback) {
+    try {
+      this.server.close(callback);
+    } catch (e) {
+      log.error('disconnect throw', e);
+    }
+    _.forEach(this.serverConnections, function closeConnection(connection) {
+      connection.destroy();
+    });
+  }
+
+  setupServerEvents() {
+    this.serverConnections = {};
+    this.nextConnectionId = 0;
+    var self = this;
+    this.server.on('connection', function ConnectionEvent(connection) {
+
+      self.nextConnectionId++;
+      var id = self.nextConnectionId;
+      self.serverConnections[id] = connection;
+
+      connection.on('close', function CloseEvent() {
+        delete self.serverConnections[id];
+      });
+    });
+  }
+
+  setupRoutes(app) {
+    app.use('/', function ApiRequestRoute(req, res, next) {
+      log.info('API request', req.path, JSON.stringify(req.body));
+      next();
+    });
+
+    app.use(function NotFoundRoute(req, res) {
+      log.info(404, req.path);
+      res.status(404);
+      res.json({'message': 'not found'});
+    });
+  }
+
+  expressApp() {
+    var app = express(),
+      adminRoute = express.Router();
+    var routes = new ExpressRoutes();
+    app.disable('x-powered-by');
+    app.enable('trust proxy');
+
+    var serviceRouteCtx = function (route) {
+      return function ServiceRoute(req, res) {
+        route(req, res);
+      };
+    };
+
+    _.keys(routes.routeDetails()).forEach(function registerServiceRoute(route) {
+      adminRoute.route('/' + route).get(serviceRouteCtx(routes[route].bind(routes)));
+    });
+
+    app.use('/_/', adminRoute);
+    return app;
+  }
+
+  defaultCallback(err, data) {
+    if (err) {
+      log.error(err, data);
+      process.exit(1);
+      return;
+    }
+    this.connect();
+  }
+
+  end() {
+    this.disconnect();
+  }
+}
+exports.Service = Service;
