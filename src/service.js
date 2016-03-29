10b9f4eafc97a0919b8f7730ac4dfb76ff372acf
Matt <matthewaveryusa@gmail.com>
2016-03-29 01:42:51 -0400

    service sets up more routes by default

--- src/service.js
+++ src/service.js
@@ -4,7 +4,10 @@ const _ = require('lodash'),
   ExpressRoutes = require('./express.js').ExpressRoutes,
   express = require('express'),
   http = require('http'),
-  log = require('totem-log');
+  log = require('totem-log'),
+  tools = require('totem-tools'),
+  cors = require('cors');
+
 
 /**
  * @module libs/service
@@ -28,8 +31,32 @@ class Service {
     log.info('starting server','__LOCATION__');
     var app = this.expressApp();
     this.server = http.createServer(app);
-    this.setupServerEvents();
+    this.setupServerEvents(app);
+
+    app.use(function (req, res, next) {
+      req.start = Date.now();
+      next();
+    });
+
+    this.setupPreRoutes(app);
     this.setupRoutes(app);
+    this.setupPostRoutes(app);
+    
+    //this is the final handler that prints metrics
+    app.use(function (req, res) {
+      var time = Date.now() - req.start;
+      log.metric({'time':time, 'status':res.statusCode, 'method': req.method, 'url': req.originalUrl, 'ip': req.ip, 'referer': req.referer},  '__LOCATION__');
+    });
+
+    //this is the final handler when all else fails
+    app.use(function (err, req, res, next) {
+        console.log(err);
+        log.error({'error':err,'stack':err.stack},'__LOCATION__');
+        res.status(500).json({'errorCode': 'internalError'});
+    });
+
+    
+
     this.server.on('error', function httpServerError(err) {
       log.error(err,'__LOCATION__');
       process.exit(1);
@@ -75,16 +102,48 @@ class Service {
     });
   }
 
+  setupPreRoutes(app) {
+    app.use(cors());
+    //if req.identity exists, it means this request is from a logged-in user.
+    app.use(function (req, res, next) {
+      var sessionId = req.headers['x-totem-session-id'];
+      if (!sessionId) {
+        return next();
+      }
+      tools.sessionExists(self.redis, sessionId, function (err, data) {
+        if (err) {
+          return next(err);
+        }
+        req.identity = {'userId': data.userId};
+        next();
+      });
+    });
+  }
+
   setupRoutes(app) {
-    app.use('/', function ApiRequestRoute(req, res, next) {
-      log.info({path: req.path, body: JSON.stringify(req.body)},'__LOCATION__');
+  }
+  
+  setupPostRoutes(app) {
+    app.use(function (req, res, next) {
+      if (!res.headersSent) {
+        res.status(404).json({'errorCode': 'notFound'});
+      }
       next();
     });
 
-    app.use(function NotFoundRoute(req, res) {
-      log.info({code: 404, path: req.path, body: JSON.stringify(req.body)},'__LOCATION__');
-      res.status(404);
-      res.json({'message': 'not found'});
+    app.use(function (err, req, res, next) {
+      if (err instanceof SyntaxError) {
+        res.status(400).json({'errorCode': 'invalidJSON'});
+        next();
+      } else if (err instanceof tools.ClientError) {
+        res.status(err.status).json({'errorCode': err.errorCode});
+        next();
+      } else if (err instanceof tools.InputError) {
+        res.status(err.status).json({'errorCode': err.errorCode, 'details': err.details});
+        next();
+      } else {
+        next(err);
+      }
     });
   }
 
@@ -106,6 +165,22 @@ class Service {
     });
 
     app.use('/_/', adminRoute);
+
+    //if req.identity exists, it means this request is from a logged-in user.
+    app.use(function (req, res, next) {
+      var sessionId = req.headers['x-totem-session-id'];
+      if (!sessionId) {
+        return next();
+      }
+      tools.sessionExists(self.redis, sessionId, function (err, data) {
+        if (err) {
+          return next(err);
+        }
+        req.identity = {'userId': Number(data.userId), 'username': data.username,'session': sessionId};
+        next();
+      });
+    });
+
     return app;
   }
 
